5e5d2083a8739f07500f5ea10a3e06dd20dbfaf8
danieloscar1808 <danieloscar1808@gmail.com>
2026-03-12 11:31:25 -0300

    se arregla para ver en github-page

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+
+  base: mode === "production" ? "/solar-diagram-builder/" : "/",
+
   server: {
     host: "::",
     port: 8080,
@@ -12,10 +15,44 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
+  plugins: [
+
+    react(),
+
+    mode === "development" && componentTagger(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Prompt",
+        short_name: "Prompt",
+        start_url: "/NOMBRE-REPOSITORIO/",
+        display: "standalone",
+        background_color: "#000000",
+        theme_color: "#000000",
+
+        icons: [
+          {
+            src: "/NOMBRE-REPOSITORIO/icon-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/NOMBRE-REPOSITORIO/icon-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+
+  ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
 }));
