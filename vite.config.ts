9e5277ab15b4fb0c3a4ebd9799fc0f91423d5c99
danieloscar1808 <danieloscar1808@gmail.com>
2026-03-12 12:02:50 -0300

    refresh docs

--- vite.config.ts
+++ vite.config.ts
@@ -34,12 +34,12 @@ export default defineConfig(({ mode }) => ({
 
         icons: [
           {
-            src: "/NOMBRE-REPOSITORIO/icon-192.png",
+            src: "icon-192.png",
             sizes: "192x192",
             type: "image/png"
           },
           {
-            src: "/NOMBRE-REPOSITORIO/icon-512.png",
+            src: "icon-512.png",
             sizes: "512x512",
             type: "image/png"
           }
